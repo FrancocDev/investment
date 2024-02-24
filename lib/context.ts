dccf1e718fe948c4b467b072cd1ad4eade48700b
Franco Carrara <frankko212@gmail.com>
2024-02-24 05:34:58 -0300

    Modify starting state value

--- lib/context.ts
+++ lib/context.ts
@@ -2,6 +2,6 @@ import { create } from 'zustand'
 import { InvestmentState } from './types'
 
 export const useInvestment = create<InvestmentState>((set) => ({
-  investment: 1000000,
+  investment: 0,
   setInvestment: (investment: number) => set({ investment }),
 }))
